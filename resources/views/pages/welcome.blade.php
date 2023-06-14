f94f0247ded33e9a77c2d13194c91fe15ab0a992
AndreaFerrini <andrea.ferrini90@gmail.com>
2023-06-14 22:01:30 +0200

    modifiche per visualizzare in pagina

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -6,4 +6,34 @@
 
 @section ( "content" )
 
+
+<div class="row">
+    @foreach ($trains as $elem)
+        <div class="col-3">
+            <div class="card">
+                <div class="card-body">
+                    <h4 class="card-title">{{ $elem->company }}</h4>
+                    <div class="row">
+                        <div class="col-6">
+                            <span>{{ $elem->departure_station }}</span>
+                        </div>
+                        <div class="col-6">
+                            <span>{{ $elem->arrival_station }}</span>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-6">
+                            <span>{{ $elem->departure_time }}</span>
+                        </div>
+                        <div class="col-6">
+                            <span>{{ $elem->arrival_time }}</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endforeach
+</div>
+
+
 @endsection
